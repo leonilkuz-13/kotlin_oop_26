d76088a45b38577ff6a69914edabdc61d0664910
Leonid <leonilfilippenkov@gmail.com>
2026-04-06 21:43:05 +0300

    adding ArrayListStack.kt

--- src/main/kotlin/stack/ArrayListStack.kt
+++ src/main/kotlin/stack/ArrayListStack.kt
@@ -1,5 +1,30 @@
 package org.example.stack
 
-class ArrayListStack : Stack {
-    // TODO: use delegation instead of inheritance here, use CustomArrayList
+import org.example.list.CustomArrayList
+import org.example.list.CustomList
+
+class ArrayListStack (private val storage: CustomList = CustomArrayList(10)) : Stack, CustomList by storage {
+    override fun push(value: Int) {
+        storage.add(value)
+    }
+
+    override fun pop(): Int {
+        if (isEmpty) {
+            throw NoSuchElementException()
+        }
+
+        val index = storage.size - 1
+        return storage.get(index)
+    }
+
+    override fun peek(): Int {
+        if (isEmpty) {
+            throw NoSuchElementException()
+        }
+
+        return storage.get(storage.size - 1)
+    }
+
+    override val isEmpty: Boolean
+        get() = storage.size == 0
 }
