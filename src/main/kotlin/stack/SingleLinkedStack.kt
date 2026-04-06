4901c50e29620f09e929377c7a7835dd0d64dbad
Leonid <leonilfilippenkov@gmail.com>
2026-04-06 21:41:49 +0300

    adding SingleLinkedStack.kt

--- src/main/kotlin/stack/SingleLinkedStack.kt
+++ src/main/kotlin/stack/SingleLinkedStack.kt
@@ -1,6 +1,23 @@
 package org.example.stack
 
+import org.example.list.SingleLinkedList
 
-class SingleLinkedStack : Stack {
-    // TODO: have to inherit from SingleLinkedStack! Add inheritance before implementation!
+class SingleLinkedStack : SingleLinkedList(), Stack {
+
+    override fun push(value: Int) {
+        addFirst(value)
+    }
+
+    override fun pop(): Int {
+        val value = get(0)
+        remove(value)
+        return value
+    }
+
+    override fun peek(): Int {
+        return get(0)
+    }
+
+    override val isEmpty: Boolean
+        get() = size == 0
 }
