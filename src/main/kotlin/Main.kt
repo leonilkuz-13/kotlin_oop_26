19e349ddc24c3617c9efd9be43352ca4040e651b
Leonid <leonilfilippenkov@gmail.com>
2026-04-09 20:30:54 +0300

    adding printer

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,42 @@
 package org.example
 
+import org.example.list.CustomArrayList
+import org.example.list.SingleLinkedList
 import org.example.list.SingleLinkedList.Companion.singleLinkedListOf
+import org.example.stack.ArrayListStack
+import org.example.stack.SingleLinkedStack
+
 
 fun main() {
     val t = singleLinkedListOf(1, 2, 3)
-//    ListPrinter.printList(t)
+
+    ListPrinter.printList(t)
+
+    val arrayList = CustomArrayList(5)
+    arrayList.add(1)
+    arrayList.add(2)
+    arrayList.add(3)
+    println("CustomArrayList:  ")
+    ListPrinter.printList(arrayList)
+
+    val linkedList = SingleLinkedList()
+    linkedList.add(1)
+    linkedList.add(2)
+    linkedList.add(3)
+    println("SingleLinkedList: ")
+    ListPrinter.printList(linkedList)
+
+    val arrayStack = ArrayListStack()
+    arrayStack.push(1)
+    arrayStack.push(2)
+    arrayStack.push(3)
+    println("ArrayListStack:   ")
+    ListPrinter.printList(arrayStack)
+
+    val linkedStack = SingleLinkedStack()
+    linkedStack.push(1)
+    linkedStack.push(2)
+    linkedStack.push(3)
+    println("SingleLinkedStack:")
+    ListPrinter.printList(linkedStack)
 }
